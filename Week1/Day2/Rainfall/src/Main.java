ad927ac5668c2308a8c8b20be36682248ec331dd
jbarto15 <joshuabarton222@gmail.com>
2023-09-27 21:50:43 -0600

    rainfall hw

---
+++ Week1/Day2/Rainfall/src/Main.java
@@ -0,0 +1,42 @@
+//Josh Barton - Rainfall HW
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        //create a variable called rainData that takes the name of the file
+        RainData rainData = new RainData("/Users/joshbarton/Desktop/MSD2023/CS6011/Week1/Day2/Rainfall/src/rainfall_data.txt");
+
+        //create an array list of type RainData and use the dataList method to get an array list of all the rain data (month, year, rainfall)
+        ArrayList<RainData> dataList = rainData.dataList();
+
+        //variable to store the statistics for each month and the overall average rainfall for all months
+        String textToAddToFile = new String();
+
+        //variable to count the overall total average rainfall
+        double sumOfAverageRainfall = 0.0;
+
+        //use a for loop from 0 to 11 since there are 12 months in a year and display the average rainfall for that month
+        for (int i = 0; i < 12; i++ ) {
+            String targetMonth = dataList.get(i).getMonth();
+            double averageRainfall = rainData.findAverageRainfall(dataList, targetMonth);
+            System.out.println("The average rainfall amount for " + targetMonth + " is " + averageRainfall);
+            sumOfAverageRainfall += averageRainfall;
+            textToAddToFile += "The average rainfall amount for " + targetMonth + " is " + averageRainfall;
+        }
+
+        //get the overall average rainfall for all of the months
+        double overallTotalRainfall = sumOfAverageRainfall / 12;
+        System.out.println("The overall average rainfall amount is " + overallTotalRainfall + " inches.");
+        textToAddToFile += "The overall average rainfall amount is " + overallTotalRainfall + " inches.";
+
+        //call the outputStatistics method to add the statistics to the file
+        rainData.outputStatistics("/Users/joshbarton/Desktop/MSD2023/CS6011/Week1/Day2/Rainfall/src/rainfall_results.txt", textToAddToFile);
+    }
+}
---
+++ Week1/Day2/Rainfall/src/RainData.java
@@ -0,0 +1,105 @@
+//Josh Barton - Rainfall Assignment
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileWriter;
+
+public class RainData {
+    private String _month;
+    private int _year;
+    private Double _rainfall;
+    public String _filePath;
+
+    public String getMonth() {
+        return _month;
+    }
+
+    public double getRainfall() {
+        return _rainfall;
+    }
+
+
+    //constructor that takes a filePath and assigns the filePath given to our class variable _filePath
+    public RainData(String filePath) throws FileNotFoundException {
+        _month = "";
+        _year = 0;
+        _rainfall = 0.0;
+        _filePath = filePath;
+    }
+
+    //constructor that takes in a month, year, and average rainfall
+    public RainData(String month, Integer year, Double rainFall) throws FileNotFoundException {
+        _month = month;
+        _year = year;
+        _rainfall = rainFall;
+    }
+
+    //method that inputs the file and puts it into an array list of type RainData
+    public ArrayList<RainData> dataList() throws FileNotFoundException {
+
+        //read in the file
+        FileInputStream fileInputStream = new FileInputStream(_filePath);
+
+        //scanner that will read in the data from the input stream
+        Scanner scanner = new Scanner(fileInputStream);
+
+        //create an Array List that stores the lines of data
+        ArrayList<RainData> dataList = new ArrayList<RainData>();
+
+        //read in the file as long as there is a line to read
+        while (scanner.hasNextLine()) {
+            //Split the line using //s+ which will split the line up at each space
+            String[] line = (scanner.nextLine()).split("\\s+");
+
+            //check to make sure the line we put into the data array has all the components, i.e. is greater than 1
+            if (line.length > 1) {
+                String month = line[0];
+                Integer year = Integer.parseInt(line[1]);
+                Double rainfall = Double.parseDouble(line[2]);
+
+                //create a new object with the data received in the input
+                RainData newData = new RainData(month, year, rainfall);
+
+                //add the new objects to the array list of type RainData
+                dataList.add(newData);
+            }
+        }
+        return dataList;
+    }
+
+
+    //method that finds the average rainfall for a particular month
+    public double findAverageRainfall(ArrayList<RainData> dataList, String month) {
+        double totalRainfall = 0.0;
+        double numOfMonths = 0;
+
+        //loop through the dataList
+        for (RainData data : dataList) {
+            //check to see if the month in the data list is equal to the month passed into the method. If so, updata total rainfall by adding the rainfall of that month
+            if (data.getMonth().equals(month)) {
+                totalRainfall += data.getRainfall();
+                //update the number of months so that we will have a number to divide by
+                numOfMonths++;
+            }
+        }
+        //return the average for the specific month given
+        return totalRainfall / numOfMonths;
+    }
+
+
+    //method that will add the averages for each month and the overall average to the text file
+    public void outputStatistics(String filePath, String statisticsToAdd) throws IOException {
+        FileWriter myWriter = new FileWriter( filePath );
+        myWriter.write( statisticsToAdd );
+        myWriter.close();
+    }
+
+}
+
+
+
