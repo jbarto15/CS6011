7bdde7744c92dadbf0005f805704972b21e0c4eb
jbarto15 <joshuabarton222@gmail.com>
2023-09-28 22:00:43 -0600

    my server hw

--- Week1/Day4/MyHttpServer/src/webServer.java
+++ Week1/Day4/MyHttpServer/src/webServer.java
@@ -5,68 +5,64 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class webServer {
-        public static void main(String[] args) throws IOException {
-            //When a request comes in, parse the client's request, and (assuming it's well formed) attempt to send the requested file from the resources folder of your project. If the file doesn't exist, send back a 404 error
+    public static void main(String[] args) throws IOException {
+        //implement a server
+        //create a server socket variable that takes in port 8080
+        ServerSocket server = new ServerSocket(8080);
 
-            //implement a server
-            //create a server socket variable that takes in port 8080
-            ServerSocket server = new ServerSocket(8080);
+        //create a string variable that stores the path to the file that will be given to the client if their requested file is not found
+        String errorFilePath = "/Users/joshbarton/Desktop/MSD2023/CS6011/Week1/Day4/MyHttpServer/resources/errorHtml.html";
+        //file variable that stores a file
+        File errorHtmlFile = new File(errorFilePath);
+        //file input stream variable that inputs the error message html file
+        FileInputStream inputErrorFile = new FileInputStream(errorHtmlFile);
 
-            //while loop that allows the server socket to always be looking for a client request
-            while (true) {
-                //create socket variable that creates a socket on the server to be able to recognize the client
-                Socket client = server.accept();
-                //use the scanner class to more easily read in the data
-                Scanner scanner = new Scanner(client.getInputStream());
+        //while loop that allows the server socket to always be looking for a client request
+        while (true) {
+            //create socket variable that creates a socket on the server to be able to recognize the client
+            Socket client = server.accept();
+            //use the scanner class to more easily read in the data from the client
+            Scanner scanner = new Scanner(client.getInputStream());
 
-                //variable to store the request from the client
-                String clientRequest = "";
+            //variable to store the request from the client
+            String clientRequest = "";
 
-                //check to see if there is input from the client, if so, print out there request
-                if (scanner.hasNext()) {
-                    //store the client request in a string variable
-                    clientRequest = scanner.nextLine();
-                    System.out.println("Client Request: " + clientRequest);
+            //check to see if there is input from the client
+            if (scanner.hasNext()) {
+                //store the client request in client request variable
+                clientRequest = scanner.nextLine();
+                System.out.println("Client Request: " + clientRequest);
 
-                    //parse the client request into its parts and store it into an array of strings
-                    String[] parts = clientRequest.split(" ");
+                //parse the client request into parts based on where there is a space and store it into an array of strings
+                String[] parts = clientRequest.split(" ");
 
-                    //store the file that is being requested in a variable
-                    System.out.println(parts[1]);
+                //create a string variable that stores the path to the file that will be given to the client
+                String filePath = "/Users/joshbarton/Desktop/MSD2023/CS6011/Week1/Day4/MyHttpServer/resources" + parts[1];
+                //create a file variable that creates a new File type and takes the filePath as its parameter
+                File htmlFile = new File(filePath);
+                //create a filestream variable that streams in the html file
+                FileInputStream inputFile = new FileInputStream(htmlFile);
 
-                    //create a file variable that stores the path to the file that will be given to the client
-                    String filePath = "/Users/joshbarton/Desktop/MSD2023/CS6011/Week1/Day4/MyHttpServer/resources" + parts[1];
-                    File htmlFile = new File(filePath);
-                    FileInputStream inputFile = new FileInputStream(htmlFile);
+                //create output stream variable that assigns the clients output stream to an OutputStream variable
+                OutputStream outputStream = client.getOutputStream();
 
-                    //create output stream variable that assigns the clients output stream to an OutputStream variable
-                    OutputStream outputStream = client.getOutputStream();
+                //check to make sure the request is a GET request and that the requested file exists
+                if (parts[0].equals("GET") && htmlFile.exists()) {
+                        //if file exists print out file found
+                        System.out.println("File found");
+                        //give the header information to the client
+                        outputStream.write("HTTP/1.1 200 OK\n".getBytes());
+                        outputStream.write("Content-Type: text/html\n".getBytes());
+                        outputStream.write("\n".getBytes());
+                        //using the transferTo method in the input FileInputStream class, transfer the requested file directly to the client output stream
+                        inputFile.transferTo(outputStream);
 
-                    //check to make sure the request has all 3 parts of getting a request from client by creating individual variables for them
-                    if (parts[0].equals("GET")) {
-                    //if (parts[0].equals("GET") && parts[1].equals(filePath) || parts[1].equals("/") && parts[2].equals( "HTTP/1.1")) {
-                        //return to the client that the specific file is found
-                        if (htmlFile.exists()) {
-                            System.out.println("File found");
-                            //give the header information to the client and the file
-                            outputStream.write("HTTP/1.1 200 OK\n".getBytes());
-                            outputStream.write("Content-Type: text/html\n".getBytes());
-                            outputStream.write("\n".getBytes());
-                            inputFile.transferTo(outputStream);
+                        //flush the output stream
+                        outputStream.flush();
 
-                            //flush the output stream
-                            outputStream.flush();
-
-                            //close the output stream
-                            outputStream.close();
-                        }
-                    } else if (!htmlFile.exists()) {
-                        //return the error that the file is not found
-                        //create a file variable that stores the path to the file that will be given to the client
-                        String errorFilePath = "/Users/joshbarton/Desktop/MSD2023/CS6011/Week1/Day4/MyHttpServer/resources/errorHtml.html";
-                        File errorHtmlFile = new File(errorFilePath);
-                        FileInputStream inputErrorFile = new FileInputStream(errorHtmlFile);
-
+                        //close the output stream
+                        outputStream.close();
+                    } else {
                         //give the header information to the client and the file
                         outputStream.write("HTTP/1.1 404 Not Found".getBytes());
                         outputStream.write("Content-Type: text/html".getBytes());
@@ -80,11 +76,7 @@ public class webServer {
                         //close the output stream
                         outputStream.close();
                     }
-
                 }
-                }
-
-
-
+            }
         }
     }
