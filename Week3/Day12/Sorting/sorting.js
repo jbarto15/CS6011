79876f5a66480f59e028a45bad84f4abafc02aa9
jbarto15 <joshuabarton222@gmail.com>
2023-10-21 15:00:22 -0600

    sorting hw

--- Week3/Day12/Sorting/sorting.js
+++ Week3/Day12/Sorting/sorting.js
@@ -1,3 +1,4 @@
+
 "use strict";
 
 //an array of ints
@@ -9,80 +10,61 @@ let stringArray = ["hello", "my", "name", "is", "Josh", "Barton"];
 //an array of a mix of these things
 let mixArray = [2.4, 4, "hello", 7.7, 1, "josh"];
 
-//function called selection sort that will take an array and sort it
-function selectionSort(array, compareTo) {
-    //define some variables that will help us sort
-    let sortedArray = new Array();
-    //use the findMinPos function and store that value as value 0 of the sorted array
-    sortedArray[0] = findMinLocation(array);
-    //loop through the array
-    for (let i = 0; i < array.length; i++) {
-        //check if the value we're on is greater than min
-        if (array[i] > sortedArray[0]) {
-            sortedArray.push(array[i]); //add array item to the back of the array
-        } else if (array[i] < sortedArray[0]) {
-            sortedArray.unshift(array[i]); //add array item to the front of the list using the unshift method
-        }
-    }
-    //return the sorted array
-    return sortedArray;
-}
-
-//call selection sort function
-console.log("Sorted Integer Array: ");
-console.log(selectionSort(integerArray));
-console.log("Sorted Float Array: ");
-console.log(selectionSort(floatArray));
-console.log("Sorted String Array: ");
-console.log(selectionSort(stringArray));
-
-
-//helper function called findMinLocation()
-function findMinLocation(array) {
-    //define some variables that will help us find min location in array
-    let minItem = array[0];
-    let minPosition = 0;
-    //loop over the array of integers
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] < minItem) {
-            minItem = array[i];
-            minPosition = i;
-        }
-    }
-    return minItem;
-}
-
-//call find min location function
-console.log(findMinLocation(integerArray));
-
-console.log(findMinLocation(floatArray));
-
-console.log(findMinLocation(stringArray));
-
-console.log(findMinLocation(mixArray));
-
-
-//comparison function that returns whether or not a is less than be
-function compareTo(a,b) {
-    //compare a and b
-
-    // If a is less than b, store the comparison 
-    if (a < b) {
-        const abComparison = a.localeCompare(b);
-    }
-
-    return abComparison;
-}
-
-//call the selectedSort function with the compareTo
-console.log(selectionSort(integerArray, compareTo));
-
-
-
 //create a person object with first and last name member variables
 let person = [{firstName: "Josh", lastName: "Barton"}, {firstName: "Brittney", lastName: "Porter"}, {firstName: "Roonie", lastName: "Barton"}];
 
 //print the person object
-console.log(person);
+//console.log(person);
+
+//function called selection sort that will take an array and sort it
+function selectionSort(array, compareTo) {
+    //loop to go through the array
+    for (let i = 0; i < array.length; i++) {
+        //store the position of the minimum value in the array
+        let minPosition = findMinLocation(array, i, compareTo);
+        //store the value of the current element in the temporary variable
+        const temp = array[i];
+        //assign current position value to the value at the min location
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+    //print out the array
+    console.log(array);
+}
 
 
+//helper function called findMinLocation()
+function findMinLocation(array, index, compareTo) {
+    //define some variables that will help us find min location in array
+    let minPosition = index;
+    //loop over the array of integers
+    for (let i = index + 1; i < array.length; i++) {
+       //check if the current element is less than the element at the min position using compareTo function
+            if (compareTo(array[i], array[minPosition])) {
+                minPosition = i;
+            }
+    }
+    return minPosition;
+}
+
+
+//function to compare two items in the array
+function compareTo(a,b) {
+    //check if a and b are of the same type
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.localeCompare(b);
+    }
+    
+    if (typeof a === typeof b) {
+        return a < b;
+        
+    } 
+
+    return 0;
+}
+
+//call my selection sort function
+selectionSort(integerArray, compareTo);
+selectionSort(floatArray, compareTo);
+selectionSort(stringArray,compareTo);
+selectionSort(mixArray, compareTo);
